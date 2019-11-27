2f4af3e7a56d9a9161be6e1bc651efe7f5a95cfc
liubaohai <baohai.liu@baishancloud.com>
2019-11-27 17:17:02 +0800

    hello world

---
+++ components/hello/tests/hello.rs
@@ -0,0 +1,6 @@
+use hello;
+
+#[test]
+fn test_add_two() {
+    assert_eq!(4, hello::add_two(2));
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use hello;
+
+fn main() {
+    println!("Hello, world!");
+    assert_eq!(4, hello::add_two(2));
+}
