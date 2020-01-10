d15edd72528a01a375b78627a3156933707da8a9
wenbo <wenbobuaa@foxmail.com>
2020-01-10 10:42:07 +0800

    reduce snapshot review: open a rocksdb

--- components/epaxos/src/snapshot/mod.rs
+++ components/epaxos/src/snapshot/mod.rs
@@ -1,24 +1,13 @@
 use rocksdb::DB;
 
-#[cfg(test)]
-#[path = "tests/snapshot_test.rs"]
-mod tests;
-
 mod errors;
 pub use errors::*;
 
-mod cfg_handle;
-pub use cfg_handle::*;
-
-mod kv_handle;
-mod inst_handle;
-mod conflict_handle;
 mod rocks;
-mod util;
 
 pub struct Engine<'a> {
-    db: DB,
-    cf: &'a str,
+    _db: DB,
+    _cf: &'a str,
 }
 
 pub enum DBPath{
--- components/epaxos/src/snapshot/rocks/mod.rs
+++ components/epaxos/src/snapshot/rocks/mod.rs
@@ -1,54 +1,27 @@
-use rocksdb::{Writable, WriteBatch};
-
 use super::{Error, Engine, DBCF, DBPath};
 
 mod dbutil;
 use dbutil::*;
 
 impl Engine<'_> {
-    pub fn new<'a>(path: DBPath, cf: DBCF) -> Engine<'a> {
-        let db = open(path).unwrap();
+    /// Open a new Engine to use snapshot.
+    ///
+    /// # Examples:
+    /// ```
+    /// use crate::epaxos::snapshot::{DBCF, DBPath, Engine};
+    ///
+    /// let my_eng;
+    /// match Engine::new(DBPath::Test, DBCF::Default) {
+    ///     Ok(eng) => my_eng = eng,
+    ///     Err(err) => println!("failed to get snapshot engine, failed: {}", err),
+    /// };
+    /// ```
+    pub fn new<'a>(path: DBPath, cf: DBCF) -> Result<Engine<'a>, Error> {
+        let db = open(path)?;
 
-        Engine{
-            db: db,
-            cf: cf.as_str(),
-        }
-    }
-
-    // Set a key-value pair to rocksdb.
-    pub fn set(&self, k: &[u8], v: &[u8]) -> Result<(), Error> {
-        let cfh = self.db.cf_handle(self.cf).unwrap();
-        Ok(self.db.put_cf(cfh, k, v)?)
-    }
-
-    // Set multi keys-values to rocksdb.
-    pub fn mset(&self, keys: &Vec<&str>, values: &Vec<&str>) -> Result<(), Error> {
-        let wb = WriteBatch::new();
-        let cfh = self.db.cf_handle(self.cf).unwrap();
-
-        let len = keys.len();
-
-        for i in 0..len {
-            let k = keys[i];
-            let v = values[i];
-
-            wb.put_cf(cfh, k.as_bytes(), v.as_bytes())?;
-        }
-
-        Ok(self.db.write(&wb)?)
-    }
-
-
-    // Get a value in rocksdb with it's key.
-    pub fn get(&self, k: &str) -> Result<String, Error> {
-        let cfh = self.db.cf_handle(self.cf).unwrap();
-
-        match self.db.get_cf(cfh, k.as_bytes()){
-            Ok(option_val) => match option_val{
-                Some(val) => {return Ok(val.to_utf8().unwrap().to_string());},
-                None => {return Err(Error::RocksDBError{msg: format!("value for key {} not found!", k)});},
-            },
-            Err(err) => {return Err(Error::RocksDBError{msg: format!("load key {} got error: {}", k, err)});},
-        };
+        Ok(Engine{
+            _db: db,
+            _cf: cf.as_str(),
+        })
     }
 }
