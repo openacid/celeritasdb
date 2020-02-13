14276d6fe49add30caa0811ae6badf72f7a37454
Restyled.io <commits@restyled.io>
2020-02-13 07:33:24 +0000

    Restyled by rustfmt

--- src/bin/cele.rs
+++ src/bin/cele.rs
@@ -8,7 +8,7 @@ use redis;
 use net2::TcpBuilder;
 use std::io;
 use std::io::{Read, Write};
-use std::net::{SocketAddr, TcpStream, ToSocketAddrs, TcpListener};
+use std::net::{SocketAddr, TcpListener, TcpStream, ToSocketAddrs};
 use std::str::from_utf8;
 
 use std::sync::mpsc::{channel, Receiver};
@@ -83,9 +83,7 @@ impl Server {
         for addr in t.to_socket_addrs()? {
             let listener = self.make_listener(addr, tcp_backlog)?;
 
-            let th = thread::spawn(move || {
-                Self::api_server_loop(listener)
-            });
+            let th = thread::spawn(move || Self::api_server_loop(listener));
             self.listener_threads.push(th);
         }
         Ok(())
@@ -108,15 +106,15 @@ impl Server {
         }
     }
 
-    fn make_listener(&mut self, addr:SocketAddr, backlog: i32) -> io::Result<TcpListener> {
-            let builder = match addr {
-                SocketAddr::V4(_) => TcpBuilder::new_v4(),
-                SocketAddr::V6(_) => TcpBuilder::new_v6(),
-            }?;
+    fn make_listener(&mut self, addr: SocketAddr, backlog: i32) -> io::Result<TcpListener> {
+        let builder = match addr {
+            SocketAddr::V4(_) => TcpBuilder::new_v4(),
+            SocketAddr::V6(_) => TcpBuilder::new_v6(),
+        }?;
 
-            self.reuse_address(&builder)?;
-            let listener = builder.bind(addr)?.listen(backlog)?;
-            Ok(listener)
+        self.reuse_address(&builder)?;
+        let listener = builder.bind(addr)?.listen(backlog)?;
+        Ok(listener)
     }
 
     /// Starts threads listening to new connections.
@@ -124,7 +122,7 @@ impl Server {
         let addresses = vec![("127.0.0.1".to_owned(), self.listen_port)];
         for (host, port) in addresses {
             self.api_listen((&host[..], port), 10).unwrap();
-            println!( "ready to accept connections on port {}", port);
+            println!("ready to accept connections on port {}", port);
         }
     }
 }
