f5d08bc625ad97e82065c517763f114226699dea
drdr xp <drdr.xp@gmail.com>
2020-02-17 15:36:23 +0800

    impl Request and Reply for all requests and replies

--- components/epaxos/src/message/tests/message_tests.rs
+++ components/epaxos/src/message/tests/message_tests.rs
@@ -1,9 +1,37 @@
 use super::*;
-use crate::instance::{BallotNum, InstanceID, Instance, InstanceStatus};
-use crate::message::{Request, Reply};
 use crate::command::{Command, OpCode};
+use crate::instance::{BallotNum, Instance, InstanceID, InstanceStatus};
+use crate::message::{Reply, Request};
+// Message is required to use to use method in trait Message.
+use protobuf::RepeatedField;
 use protobuf::{parse_from_bytes, Message};
-use protobuf::{RepeatedField};
+
+fn new_foo_inst() -> Instance {
+    let replica = 1;
+
+    let inst_id1 = InstanceID::new_instance_id(1, 10);
+    let inst_id2 = InstanceID::new_instance_id(2, 20);
+    let inst_id3 = InstanceID::new_instance_id(3, 30);
+    let initial_deps = vec![inst_id1.clone(), inst_id2.clone(), inst_id3.clone()];
+
+    let status = InstanceStatus::NA;
+
+    let cmd1 = Command::new_command(OpCode::NoOp, "k1", "v1");
+    let cmd2 = Command::new_command(OpCode::Get, "k2", "v2");
+    let cmds = vec![cmd1, cmd2];
+
+    let ballot = BallotNum::new_ballot_num(0, 0, replica);
+    let ballot2 = BallotNum::new_ballot_num(1, 2, replica);
+
+    let mut inst = Instance::new_instance(status, &cmds[..], &ballot, &initial_deps[..]);
+    // TODO move these to Instance::new_instance
+    inst.set_instance_id(inst_id1);
+    inst.set_deps(RepeatedField::from_slice(&[inst_id2]));
+    inst.set_final_deps(RepeatedField::from_slice(&[inst_id3]));
+    inst.set_last_ballot(ballot2);
+
+    inst
+}
 
 #[test]
 fn test_message_protobuf() {
@@ -39,5 +67,22 @@ fn test_message_protobuf() {
     assert_eq!(msg_type, msg2.msg_type);
 
     let pr2 = parse_from_bytes::<PrepareReq>(&msg2.data).unwrap();
+    assert_eq!(pr1, pr2);
+}
+
+#[test]
+fn test_request_prepare_protobuf() {
+    let req_type = RequestType::Prepare;
+    let msg_type = MessageType::TypeRequest;
+
+    let inst = new_foo_inst();
+
+    let pr1 = Request::prepare(&inst);
+    let pr_bytes: Vec<u8> = pr1.write_to_bytes().unwrap();
+    let size = pr1.compute_size();
+
+    assert_eq!(true, size > 0);
+
+    let pr2 = parse_from_bytes::<Request>(&pr_bytes).unwrap();
     assert_eq!(pr1, pr2);
 }
