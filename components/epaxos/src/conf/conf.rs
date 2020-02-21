f6b29f8071c6d10c45f9e6aca612e21bfe71b9b6
Li, Shulong <shulong.li@baishancloud.com>
2020-02-21 09:16:11 +0800

    feature: use rust yaml to parse cluster conf

--- components/epaxos/src/conf/conf.rs
+++ components/epaxos/src/conf/conf.rs
@@ -1,5 +1,11 @@
+use yaml_rust;
+
 use std::collections::HashMap;
-use std::net::IpAddr;
+use std::fs;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::path::PathBuf;
+use std::str::FromStr;
+use yaml_rust::{yaml::Array, yaml::Yaml, YamlLoader};
 
 #[cfg(test)]
 #[path = "./tests/conf_tests.rs"]
@@ -10,11 +16,122 @@ pub type NodeID = String;
 /// a struct to represent a cluster node, not necessary a replica
 pub struct NodeInfo {
     pub node_id: NodeID,
-    pub ip: IpAddr,
+    pub req_addr: SocketAddr,
+    pub req_uaddr: Option<String>,
+    pub dup_addr: SocketAddr,
     // idc: String, // TODO(lsl): need topology information of a node
 }
 
 pub struct ClusterInfo {
-    // pub nodes: Vec<NodeInfo>,
-    pub nodes: HashMap<NodeID, NodeInfo>,
+    pub nodes: HashMap<String, NodeInfo>,
+}
+
+impl ClusterInfo {
+    pub fn new(path: &PathBuf) -> Result<ClusterInfo, String> {
+        // read from configuration file to load conf
+        let content = match fs::read_to_string(path) {
+            Ok(c) => c,
+            Err(e) => return Err(e.to_string()),
+        };
+
+        let yamls = match YamlLoader::load_from_str(&content) {
+            Ok(y) => y,
+            Err(e) => return Err(e.to_string()),
+        };
+
+        return Ok(ClusterInfo {
+            nodes: match ClusterInfo::parse_nodes(&yamls[0]) {
+                Ok(n) => n,
+                Err(err_msg) => return Err(err_msg),
+            },
+        });
+    }
+
+    fn parse_nodes(raw: &Yaml) -> Result<HashMap<String, NodeInfo>, String> {
+        let mut nodes = HashMap::new();
+        let nodes_vec = match raw["nodes"].as_vec() {
+            None => return Err(String::from("no nodes info found")),
+            Some(v) => v,
+        };
+
+        for node in nodes_vec {
+            let node_hash = match node.as_hash() {
+                None => return Err(String::from("invalid node hash")),
+                Some(h) => h,
+            };
+
+            let req_addr = match node_hash.get(&Yaml::String("req".to_string())) {
+                None => return Err(String::from("req address is required")),
+                Some(r) => {
+                    match r.as_vec() {
+                        None => return Err(String::from("invalid req socket address")),
+                        Some(v) => v,
+                    }
+                },
+            };
+
+            let req_sa = match ClusterInfo::parse_socket_addr(req_addr) {
+                Ok(v) => v,
+                Err(e) => return Err(e.to_string()),
+            };
+
+            let req_ua = match node_hash.get(&Yaml::String("req_unix".to_string())) {
+                None => None,
+                Some(v) => match v.as_str() {
+                    None => return Err(format!("invalid req unix socket: {:?}", v)),
+                    Some(v) => Some(String::from(v)),
+                },
+            };
+
+            let dup_addr = match node_hash.get(&Yaml::String("dup".to_string())) {
+                None => return Err(String::from("dup socket address is required")),
+                Some(r) => {
+                    match r.as_vec() {
+                        None => return Err(String::from("invalid dup socket address")),
+                        Some(v) => v,
+                    }
+                },
+            };
+
+            let dup_sa = match ClusterInfo::parse_socket_addr(dup_addr) {
+                Ok(v) => v,
+                Err(e) => return Err(e.to_string()),
+            };
+
+            let key = req_sa.to_string();
+            let info = NodeInfo {
+                node_id: ClusterInfo::make_node_id(""),
+                req_addr: req_sa,
+                req_uaddr: req_ua,
+                dup_addr: dup_sa,
+            };
+
+            nodes.insert(key, info);
+        }
+
+        return Ok(nodes);
+    }
+
+    fn parse_socket_addr(addr: &Array) -> Result<SocketAddr, String> {
+        let ip = match addr[0].as_str() {
+            None => return Err(format!("invalid ip addr: {:?}", addr)),
+            Some(ip) => ip,
+        };
+        let port = match addr[1].as_i64() {
+            None => return Err(format!("invalid port: {:?}", addr)),
+            Some(port) => port as u16,
+        };
+
+        return Ok(SocketAddr::new(IpAddr::V4(Ipv4Addr::from_str(ip).unwrap()), port));
+    }
+
+    // make a node id from key, i.e. mac address
+    pub fn make_node_id(_: &str) -> NodeID {
+        // TODO: make sure the way to make node id
+        String::from("")
+    }
+
+    pub fn get_node(&self, ip: &str, port: u16) -> Option<&NodeInfo> {
+        self.nodes.get(&format!("{}:{}", ip, port))
+    }
 }
--- components/epaxos/src/conf/tests/conf_tests.rs
+++ components/epaxos/src/conf/tests/conf_tests.rs
@@ -1,6 +1,47 @@
 use super::*;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
 
 #[test]
 fn test_conf() {
-    println!("lsl-debug: conf_tests");
+    // create tmp yaml file
+    let content = "
+nodes:
+    - {req: [\"127.0.0.1\", 3331], dup: [\"127.0.0.1\", 4441]}
+    - {req: [\"192.168.0.1\", 3332], dup: [\"192.168.0.1\", 4442], req_unix: /var/run/usocket2}
+";
+
+    let pb = PathBuf::from("/tmp/conf.yaml");
+    let mut file = File::create(&pb).unwrap();
+    file.write_all(content.as_bytes()).unwrap();
+    file.sync_all().unwrap();
+
+    // test `new`
+    let ci = ClusterInfo::new(&pb).unwrap();
+    assert_eq!(2, ci.nodes.len());
+
+    // test `get`
+    let node = ci.get_node("127.0.0.1", 3331).unwrap();
+    assert_eq!("", node.node_id);
+    assert_eq!(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), node.req_addr.ip());
+    assert_eq!(3331, node.req_addr.port());
+    assert_eq!(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), node.dup_addr.ip());
+    assert_eq!(4441, node.dup_addr.port());
+    assert_eq!(true, node.req_uaddr.is_none());
+
+    let node = ci.get_node("192.168.0.1", 3332).unwrap();
+    assert_eq!("", node.node_id);
+    assert_eq!(IpAddr::V4(Ipv4Addr::new(192, 168, 0, 1)), node.req_addr.ip());
+    assert_eq!(3332, node.req_addr.port());
+    assert_eq!(IpAddr::V4(Ipv4Addr::new(192, 168, 0, 1)), node.dup_addr.ip());
+    assert_eq!(4442, node.dup_addr.port());
+    assert_eq!("/var/run/usocket2", node.req_uaddr.as_ref().unwrap());
+
+    let node = ci.get_node("192.168.0.1", 4442);
+    assert!(node.is_none());
+    let node = ci.get_node("127.0.0.1", 4441);
+    assert!(node.is_none());
+
+    fs::remove_file(pb).unwrap();
 }
