b26cdb8b3cac74a0675d7e64ef42bb4ff1752874
drdr xp <drdr.xp@gmail.com>
2020-03-09 00:25:22 +0800

    feat: add error InvalidRequest; convert Error for transport
    -   Add qpaxos proto error InvalidRequest to respond a request error.
    
    -   Add two snapshot errors as func return value.
    
    -   Add internal error to qpaxos proto error converter: Error::to_qerr().

--- components/epaxos/src/snapshot/errors.rs
+++ components/epaxos/src/snapshot/errors.rs
@@ -1,3 +1,5 @@
+use crate::qpaxos::{InvalidRequest, QError, StorageFailure};
+
 quick_error! {
     /// Errors occur when set/get with snapshot
     #[derive(Debug, PartialEq)]
@@ -6,6 +8,57 @@ quick_error! {
             from(msg: String) -> {msg: msg}
             display("got db error:{}", msg)
         }
+
         NotFound{}
+
+        NoSuchReplica{
+            replica_id: i64,
+            my_replica_id: i64,
+        } {
+            // from(msg: String) -> {msg: msg}
+            display("no such replica:{}, my replica_id:{}",
+                    replica_id, my_replica_id)
+        }
+        LackOf(field: String) {
+            display("lack of required field:{}",
+                    field)
+        }
+    }
+}
+
+impl Error {
+    pub fn to_qerr(&self) -> QError {
+        match self {
+            Error::DBError { msg: e } => QError {
+                sto: Some(StorageFailure {}),
+                ..Default::default()
+            },
+
+            Self::NotFound {} => QError {
+                sto: Some(StorageFailure {}),
+                ..Default::default()
+            },
+
+            Self::NoSuchReplica {
+                replica_id: rid,
+                my_replica_id: mrid,
+            } => QError {
+                req: Some(InvalidRequest {
+                    field: "to_replica_id".into(),
+                    problem: "NotFound".into(),
+                    ctx: "".into(),
+                }),
+                ..Default::default()
+            },
+
+            Self::LackOf(f) => QError {
+                req: Some(InvalidRequest {
+                    field: f.clone(),
+                    problem: "LackOf".into(),
+                    ctx: "".into(),
+                }),
+                ..Default::default()
+            },
+        }
     }
 }
