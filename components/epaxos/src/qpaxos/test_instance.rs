77604d5ad4af4c6b2ff298d3709ea91671000cd7
drdr xp <drdr.xp@gmail.com>
2020-03-17 19:02:55 +0800

    feat: add InstanceIdVec as wrapper of Vec<InstanceId>
    -   InstanceIdVec impl trait Deref thus it can be used the same way as
        with `Vec<InstanceId>`.
    
    -   InstanceIdVec also impl trait Index, to get InstanceId by ReplicaID
        just like accessing a Vec: `instance_id_vec[replica_id]`.
    
        It panics if specified replica id not found, just like accessing an
        absent key on a BTreeMap.
    
    -   InstanceIdVec provides a `get(rid: ReplicaID)` method to safely get
        an `Option<InstanceID>`.
    
    -   If InstanceIdVec contains more than one InstanceId with the same
        replica_id, only the first one is returned for `[]` and `get()`.

--- components/epaxos/src/qpaxos/test_instance.rs
+++ components/epaxos/src/qpaxos/test_instance.rs
@@ -36,3 +36,69 @@ fn test_instance_conflict() {
 
     assert!(gxsy.conflict(&gxsy));
 }
+
+#[test]
+fn test_instance_id_vec_deref() {
+    let ids = InstanceIdVec {
+        ids: vec![(1, 2).into(), (3, 4).into()],
+    };
+
+    let mut it = ids.iter();
+    assert_eq!(&ids.ids[0], it.next().unwrap());
+    assert_eq!(&ids.ids[1], it.next().unwrap());
+    assert_eq!(None, it.next());
+
+    let mut ids = InstanceIdVec {
+        ids: vec![(1, 2).into(), (3, 4).into()],
+    };
+
+    let mut it = ids.iter_mut();
+    assert_eq!(&InstanceID::from((1, 2)), it.next().unwrap());
+    assert_eq!(&InstanceID::from((3, 4)), it.next().unwrap());
+    assert_eq!(None, it.next());
+}
+
+#[test]
+fn test_instance_id_vec_index() {
+    let ids = InstanceIdVec {
+        ids: vec![(1, 2).into(), (3, 4).into()],
+    };
+
+    assert_eq!(ids.ids[0], ids[1]);
+    assert_eq!(ids.ids[1], ids[3]);
+}
+
+#[test]
+#[should_panic(expect = "NotFound instance_id with replica_id=2")]
+fn test_instance_id_vec_index_panic() {
+    let ids = InstanceIdVec {
+        ids: vec![(1, 2).into(), (3, 4).into()],
+    };
+
+    let _ = ids[2];
+}
+
+#[test]
+fn test_instance_id_vec_get() {
+    let ids = InstanceIdVec {
+        ids: vec![(1, 2).into(), (3, 4).into()],
+    };
+
+    assert_eq!(ids.ids[0], ids.get(1).unwrap());
+    assert_eq!(ids.ids[1], ids.get(3).unwrap());
+    assert_eq!(None, ids.get(2));
+}
+
+#[test]
+fn test_instance_id_vec_with_dup() {
+    let ids = InstanceIdVec {
+        ids: vec![(1, 2).into(), (3, 4).into(), (1, 100).into()],
+    };
+
+    assert_eq!(ids.ids[0], ids.get(1).unwrap());
+    assert_eq!(ids.ids[1], ids.get(3).unwrap());
+    assert_eq!(None, ids.get(2));
+
+    assert_eq!(ids.ids[0], ids[1]);
+    assert_eq!(ids.ids[1], ids[3]);
+}
