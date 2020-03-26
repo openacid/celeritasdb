9acf6558d0828ec460e3063adbd458c54fa78977
drdr xp <drdr.xp@gmail.com>
2020-03-26 14:50:16 +0800

    feat: add qpaxos::quorums.rs to calculate quorum number.
    Calculating quorum number should not a task of a replica.

---
+++ components/epaxos/src/qpaxos/test_quorums.rs
@@ -0,0 +1,22 @@
+use super::quorums::*;
+
+#[test]
+fn test_quorums() {
+    let cases: Vec<(i32, i32, i32)> = vec![
+        (0, 1, 0),
+        (1, 1, 0),
+        (2, 2, 1),
+        (3, 2, 2),
+        (4, 3, 2),
+        (5, 3, 3),
+        (6, 4, 4),
+        (7, 4, 5),
+        (8, 5, 5),
+        (9, 5, 6),
+    ];
+
+    for (n_replicas, q, fastq) in cases {
+        assert_eq!(q, quorum(n_replicas), "quorum n={}", n_replicas);
+        assert_eq!(fastq, fast_quorum(n_replicas), "fast-quorum n={}", n_replicas);
+    }
+}
