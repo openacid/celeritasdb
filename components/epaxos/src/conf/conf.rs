389bde52693a97e3ba08eeffa9bff9c56b11b4e0
drdr xp <drdr.xp@gmail.com>
2020-04-01 17:37:44 +0800

    refactor: conf: make node id.
    -   node_id is set to the key of a node in clusterinfo.
    
    -   And node id as an optional replication address.
    
    -   Refine ConfError: from 3 error:std::io::Error when reading yaml.
        serde_yaml::Error when parsing yaml and from a malformed replication
        addr.
    
    -   Deref ClusterInfo into ClusterInfo.nodes thus it is much easier to
        use.

--- components/epaxos/src/conf/conf.rs
+++ components/epaxos/src/conf/conf.rs
@@ -1,7 +1,9 @@
 use std::collections::BTreeMap;
 use std::fs;
 use std::net::SocketAddr;
+use std::net::AddrParseError;
 use std::path::PathBuf;
+use std::ops::{Deref, DerefMut};
 
 use super::errors::ConfError;
 
@@ -11,11 +13,14 @@ use serde::{Deserialize, Serialize};
 #[path = "./tests/conf_tests.rs"]
 mod tests;
 
+/// NodeID is the global identity of a service.
+/// A physical server could have several node on it.
+/// A node has one or more Replica it serves for.
 pub type NodeID = String;
 
-/// a struct to represent a cluster node, not necessary a replica
+/// Node is a struct to represent a cluster node, not necessary a replica.
 #[derive(Serialize, Deserialize, Debug)]
-pub struct NodeInfo {
+pub struct Node {
     #[serde(default)]
     pub node_id: NodeID,
     pub api_addr: SocketAddr,
@@ -26,24 +31,48 @@ pub struct NodeInfo {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ClusterInfo {
-    pub nodes: BTreeMap<String, NodeInfo>,
+    /// The key is NodeID and should be unique globally.
+    /// And when parsing cluster conf yaml, it tries to convert the key:NodeID to replication addr.
+    /// In this case a user does not need to fill in `replication`.
+    //
+    // TODO: graceful handling replication addr in `key`: e.g. only when replication is None, or
+    // make `replication` an vector.
+    pub nodes: BTreeMap<String, Node>,
+}
+
+// let user to use c.get() just like c.nodes.get()
+impl Deref for ClusterInfo {
+    type Target = BTreeMap<String, Node>;
+    fn deref(&self) -> &Self::Target {
+        &self.nodes
+    }
+}
+
+// let user to use c.get() just like c.nodes.get()
+impl DerefMut for ClusterInfo {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.nodes
+    }
 }
 
 impl ClusterInfo {
-    pub fn new(path: &PathBuf) -> Result<ClusterInfo, ConfError> {
+    /// from_file read cluster conf yaml from a local file.
+    pub fn from_file(path: &PathBuf) -> Result<ClusterInfo, ConfError> {
         let content = fs::read_to_string(path)?;
         let mut cluster: ClusterInfo = serde_yaml::from_str(content.as_str())?;
 
-        for (_, node) in cluster.nodes.iter_mut() {
-            node.node_id = ClusterInfo::make_node_id("");
+        for (nid, node) in cluster.nodes.iter_mut() {
+            ClusterInfo::norm_node(nid, node)?;
         }
 
         return Ok(cluster);
     }
 
+    // TODO test bad node id as replication addr
     // make a node id from key, i.e. mac address
-    pub fn make_node_id(_: &str) -> NodeID {
-        // TODO: make sure the way to make node id
-        return String::from("");
+    pub fn norm_node(nid: &str, node: &mut Node) -> Result<(), AddrParseError> {
+        node.node_id = String::from(nid);
+        node.replication = nid.parse()?;
+        Ok(())
     }
 }
