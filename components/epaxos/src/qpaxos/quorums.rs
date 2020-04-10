eefee436021492e28d6ed2f48f129ca1c1b34523
drdr xp <drdr.xp@gmail.com>
2020-04-10 15:12:52 +0800

    fix: bug of fast-quorum: it must be greater than quorum

--- components/epaxos/src/qpaxos/quorums.rs
+++ components/epaxos/src/qpaxos/quorums.rs
@@ -5,5 +5,12 @@ pub fn quorum(n: i32) -> i32 {
 pub fn fast_quorum(n: i32) -> i32 {
     let q = n / 2 + 1;
     let f = (n - 1) / 2;
-    f + q / 2
+    let fq = f + q / 2;
+    // Except f + q/2, fast_quorum must satisfy another condition:
+    // two fast_quorum must have intersection.
+    if fq < q {
+        q
+    } else {
+        fq
+    }
 }
