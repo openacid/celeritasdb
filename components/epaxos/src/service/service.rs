c39da3354658d8d2e33deb5ac0999ce2018c547d
drdr xp <drdr.xp@gmail.com>
2020-04-12 19:46:19 +0800

    feat: QPaxos service impl add server_data thus it actually works for fast-accept, accept and commit now.

--- components/epaxos/src/service/service.rs
+++ components/epaxos/src/service/service.rs
@@ -6,10 +6,12 @@ use crate::qpaxos::FastAcceptReply;
 use crate::qpaxos::FastAcceptRequest;
 use crate::qpaxos::Instance;
 use crate::qpaxos::MakeReply;
-use crate::qpaxos::MakeRequest;
 use crate::qpaxos::PrepareReply;
 use crate::qpaxos::PrepareRequest;
+use crate::qpaxos::ProtocolError;
 use crate::qpaxos::QPaxos;
+use crate::qpaxos::RequestCommon;
+use crate::replica::Replica;
 use crate::ServerData;
 use std::sync::Arc;
 
@@ -25,6 +27,13 @@ impl MyQPaxos {
     pub fn new(server_data: Arc<ServerData>) -> Self {
         MyQPaxos { server_data }
     }
+    pub fn get_replica(&self, cmn: &Option<RequestCommon>) -> Result<&Replica, ProtocolError> {
+        let cmn = cmn.as_ref().ok_or(ProtocolError::LackOf("cmn".into()))?;
+        let rid = cmn.to_replica_id;
+        let r = self.server_data.local_replicas.get(&rid);
+        let r = r.ok_or(ProtocolError::NoSuchReplica(rid, 0))?;
+        Ok(r)
+    }
 }
 
 #[tonic::async_trait]
@@ -33,13 +42,21 @@ impl QPaxos for MyQPaxos {
         &self,
         request: Request<FastAcceptRequest>,
     ) -> Result<Response<FastAcceptReply>, Status> {
-        // TODO I did nothing but let the test pass happily
-        let inst = Instance {
-            ..Default::default()
+        println!("Got a request: {:?}", request);
+        let req = request.get_ref();
+        let r = self.get_replica(&req.cmn);
+        let r = match r {
+            Ok(v) => v,
+            Err(e) => {
+                let reply = FastAcceptReply {
+                    err: Some(e.into()),
+                    ..Default::default()
+                };
+                return Ok(Response::new(reply));
+            }
         };
 
-        let reply = MakeReply::fast_accept(&inst, &[]);
-        println!("Got a request: {:?}", request);
+        let reply = r.handle_fast_accept(request.get_ref());
         Ok(Response::new(reply))
     }
 
@@ -47,13 +64,20 @@ impl QPaxos for MyQPaxos {
         &self,
         request: Request<AcceptRequest>,
     ) -> Result<Response<AcceptReply>, Status> {
-        // TODO I did nothing but let the test pass happily
-        let inst = Instance {
-            ..Default::default()
-        };
-
-        let reply = MakeReply::accept(&inst);
         println!("Got a request: {:?}", request);
+        let req = request.get_ref();
+        let r = self.get_replica(&req.cmn);
+        let r = match r {
+            Ok(v) => v,
+            Err(e) => {
+                let reply = AcceptReply {
+                    err: Some(e.into()),
+                    ..Default::default()
+                };
+                return Ok(Response::new(reply));
+            }
+        };
+        let reply = r.handle_accept(request.get_ref());
         Ok(Response::new(reply))
     }
 
@@ -61,13 +85,20 @@ impl QPaxos for MyQPaxos {
         &self,
         request: Request<CommitRequest>,
     ) -> Result<Response<CommitReply>, Status> {
-        // TODO I did nothing but let the test pass happily
-        let inst = Instance {
-            ..Default::default()
-        };
-
-        let reply = MakeReply::commit(&inst);
         println!("Got a request: {:?}", request);
+        let req = request.get_ref();
+        let r = self.get_replica(&req.cmn);
+        let r = match r {
+            Ok(v) => v,
+            Err(e) => {
+                let reply = CommitReply {
+                    err: Some(e.into()),
+                    ..Default::default()
+                };
+                return Ok(Response::new(reply));
+            }
+        };
+        let reply = r.handle_commit(request.get_ref());
         Ok(Response::new(reply))
     }
 
